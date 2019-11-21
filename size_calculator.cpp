a6a78c0b9d8983e9f3205fee29708de03fa7e327
Andrew Snow <asnow4u@gmail.com>
2019-11-20 17:28:29 -0800

    finished. needs testing

--- size_calculator.cpp
+++ size_calculator.cpp
@@ -1,5 +1,7 @@
 
 #include <stdio.h>
+#include <cstdlib>
+#include <iostream>
 #include <math.h>
 
 //Exponent
@@ -8,22 +10,35 @@
 
 int main(int argc, char* argv[]){
 
-  int associativity = argv[0];
-  int tag = argv[1];
-  int index = argv[2];
-  int offset = argv[3];
+  int associativity;
+  int tag;
+  int index;
+  int offset;
 
   int block_size;
   int num_blocks;
-  int cacheBits;
-
-  std::cout << argc << std::endl;
-  
+  int cache_bits;
+    
   //check that the proper number of arguments are present
-  if (argc == 4){
+  if (argc == 5){
+    
+    associativity = atoi(argv[1]);
+    tag = atoi(argv[2]);
+    index = atoi(argv[3]);
+    offset = atoi(argv[4]);
 
     block_size = pow(2, offset);
     num_blocks = pow(2, index) * associativity;
+    cache_bits = (1 + tag + block_size * pow(2, index)) * num_blocks;
 
+    std::cout << std::endl;
+    std::cout << "Each block requires " << block_size << " bytes." << std::endl;
+    std::cout << "This cache can hold up to " << num_blocks << " blocks." << std::endl;
+    std::cout << cache_bits << " bits of storage would be required to implement this cache." << std::endl;
+    std::cout << std::endl;
+
+  } else {
+    
+    std::cerr << "Wrong number of arguments" << std::endl;
   }
 }
